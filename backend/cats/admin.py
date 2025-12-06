fad15dcdb0f2b41b4dbc4d894c65b27d98c5cd93
Valeria Avdeykina <117504017+fleshbound@users.noreply.github.com>
2025-12-07 01:05:22 +0300

    Fix imports in admin

--- backend/cats/admin.py
+++ backend/cats/admin.py
@@ -1,5 +1,6 @@
 from django.contrib import admin
-from .models import Achievement, Cat, AchievementCat
+
+from .models import Achievement, AchievementCat, Cat
 
 admin.site.register(Achievement)
 admin.site.register(Cat)
